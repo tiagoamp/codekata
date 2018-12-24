06a6e78b37f600ae1ba84dd90101480828779d51
tiagoamp <tiagoamp@gmail.com>
2018-12-24 16:18:08 -0200

    Add comments to 'desafio'

--- java/src/code/desafios/DesafioCodeNation.java
+++ java/src/code/desafios/DesafioCodeNation.java
@@ -5,8 +5,7 @@ import java.util.List;
 
 public class DesafioCodeNation {
 
-	/* Faça um método @Desafio("Fibonacci") que retorne uma lista java.util.List 
-	 * com os primeiro números Integer de série de Fibonacci até passar de 350.
+	/* Método que retorne uma lista java.util.List com os primeiro números Integer de série de Fibonacci até passar de 350.
 	 * A série de Fibonacci é a seguinte: 0, 1, 1, 2, 3, 5, 8, 13, etc... 
 	 */
 	public List<Integer> fibonacciUntil350() {
@@ -22,9 +21,8 @@ public class DesafioCodeNation {
 		return result;
 	}
 	
-	/* Faça um segundo método @Desafio("isFibonnaci") que recebe um Integer como parâmetro e 
-	 * retorne Boolean. Caso o número recebido por parâmetro esteja entre os números da 
-	 * sequência de Fibonnaci do @Desafio("Fibonnaci") retorne true, senão, false.
+	/* Método que recebe um Integer como parâmetro e retorne Boolean. 
+	 * Caso o número recebido por parâmetro esteja entre os números da sequência de Fibonnaci retorne true, senão, false.
 	 */
 	public Boolean isFibonacci(Integer n) {
 		List<Integer> sequence = fibonacciUntil350();
