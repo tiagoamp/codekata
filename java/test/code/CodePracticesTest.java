00a17041aedb34214cdb08f30c09209665297594
tiago.albuquerque <tiagoamp@>
2019-03-14 13:26:58 -0300

    Adjust spacing

--- java/test/code/CodePracticesTest.java
+++ java/test/code/CodePracticesTest.java
@@ -168,8 +168,7 @@ public class CodePracticesTest {
 		s = Arrays.asList(2, 5, 1, 3, 4, 4, 3, 5, 1, 1, 2, 1, 4, 1, 3, 3, 4, 2, 1);
 		d = 18; m = 7;
 		result = birthdayChoc(s, d, m);
-		assertEquals(3, result);
-		
+		assertEquals(3, result);		
 	}
 
 }
