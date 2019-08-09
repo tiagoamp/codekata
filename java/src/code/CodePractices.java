56f7e26ae0854411ddd2bb3776875e0b900706ba
tiago.albuquerque <tiagoamp@>
2019-08-09 09:08:46 -0300

    Implement yet another java code practice

--- java/src/code/CodePractices.java
+++ java/src/code/CodePractices.java
@@ -268,7 +268,6 @@ public class CodePractices {
         { {2,7,6,9,5,1,4,3,8}, {6,7,2,1,5,9,8,3,4}, {8,3,4,1,5,9,6,7,2}, {4,3,8,9,5,1,2,7,6},
           {6,1,8,7,5,3,2,9,4}, {2,9,4,7,5,3,6,1,8}, {4,9,2,3,5,7,8,1,6}, {8,1,6,3,5,7,4,9,2}
         };
-
         for(int i=0;i<magicSquares.length;i++) {        
         	costs[i] = Math.abs(magicSquares[i][0]-s[0][0]) + Math.abs(magicSquares[i][1]-s[0][1]) + Math.abs(magicSquares[i][2]-s[0][2]);
         	costs[i] = costs[i] + Math.abs(magicSquares[i][3]-s[1][0]) + Math.abs(magicSquares[i][4]-s[1][1]) + Math.abs(magicSquares[i][5]-s[1][2]);
