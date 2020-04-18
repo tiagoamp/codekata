2ab64db5d5c2e96ef0b7e9cb1decc079d33c26a3
tiagoamp <tiagoamp@gmail.com>
2020-04-18 20:20:47 -0300

    Implement kotlin code practice

--- kotlin/src/code/CodePractices.kt
+++ kotlin/src/code/CodePractices.kt
@@ -1,5 +1,29 @@
 package code
 
+fun countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: IntArray, oranges: IntArray): IntArray {
+    var appleCount = 0
+    for (i in apples.indices) {
+        val distance = apples[i] + a
+        if (distance in s..t) appleCount++
+    }
+    var orangeCount = 0
+    for (i in oranges.indices) {
+        val distance = oranges[i] + b
+        if (distance in s..t) orangeCount++
+    }
+    return intArrayOf(appleCount, orangeCount)
+}
+
+fun superdigit(n: String, k: Int) : Int {
+    if (n.length <= 1) return n.toInt()
+    var sum = 0
+    for (ch in n) {
+        val digit: Int = Character.getNumericValue(ch)
+        sum += digit
+    }
+    if (k > 1) sum *= k
+    return superdigit(sum.toString(), 1)
+}
 
 fun bestDivisor(input: Int) : Int {
     val divisors = (1..input).filter { input % it == 0 }
--- kotlin/src/code/CodePracticesKtTest.kt
+++ kotlin/src/code/CodePracticesKtTest.kt
@@ -1,11 +1,47 @@
 package code
 
-import org.junit.jupiter.api.Assertions.*
-import code.*;
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 internal class CodePracticesKtTest {
+
+    // https://www.hackerrank.com/challenges/apple-and-orange/problem
+    @Test
+    fun testCountApplesAndOranges() {
+        // sample case
+        var s = 7; var t = 11 // house
+        var a = 5; var b = 15 // tree
+        var apples = intArrayOf(-2, 2, 1)
+        var oranges = intArrayOf(5, -6) // fruits array
+        var result = countApplesAndOranges(s, t, a, b, apples, oranges)
+        assertEquals(1, result[0])
+        assertEquals(1, result[1])
+        // test case 2
+        s = 2; t = 3; a = 1; b = 5
+        apples = intArrayOf(2)
+        oranges = intArrayOf(-2)
+        result = countApplesAndOranges(s, t, a, b, apples, oranges)
+        assertEquals(1, result[0])
+        assertEquals(1, result[1])
+    }
+
+    // https://www.hackerrank.com/challenges/super-digit/problem
+    @Test
+    fun `test superdit`() {
+        var n = "148"; var k = 3; var expected = 3
+        var result = superdigit(n,k)
+        assertEquals(expected, result)
+        n = "9875"; k = 4; expected = 8
+        result = superdigit(n,k)
+        assertEquals(expected, result)
+        n = "123"; k = 3; expected = 9
+        result = superdigit(n,k)
+        assertEquals(expected, result)
+        n = "100000"; k = 100000  // superior threshold
+        result = superdigit(n,k)
+        assertTrue(result > 0)
+    }
 
     // https://www.hackerrank.com/challenges/best-divisor/problem
     @Test
