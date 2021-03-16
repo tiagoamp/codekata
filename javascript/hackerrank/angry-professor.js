e7b76e56b9996f549a4065528f7382a3e931efd8
tiagoamp <tiagoamp@gmail.com>
2021-03-16 09:05:43 -0300

    Solve JS code challenge

---
+++ javascript/hackerrank/angry-professor.js
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/challenges/angry-professor/problem
+
+function angryProfessor(k, a) {
+    const ontTime = a.filter(n => n<=0).length;
+    return ontTime >= k ? "NO" : "YES";
+}
+
+
+test("sample A", () => {
+	const k = 3, a = [-1, -3, 4, 2], expected = "YES";
+    const result = angryProfessor(k, a);
+	expect(result).toBe(expected);
+});
+
+test("sample B", () => {
+	const k = 2, a = [0, -1, 2, 1], expected = "NO";
+    const result = angryProfessor(k, a);
+	expect(result).toBe(expected);
+});
+
+test("edge cases", () => {
+    const k = 2, a = [0, -100, 100], expected = "NO";
+    const result = angryProfessor(k, a);
+	expect(result).toBe(expected);
+});
