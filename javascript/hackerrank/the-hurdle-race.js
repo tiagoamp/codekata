925d5b9deddffc72871bdff7d59b3c0621bb9c04
tiagoamp <tiagoamp@gmail.com>
2021-04-16 14:27:48 -0300

    Refactor JS code challenges

--- javascript/hackerrank/the-hurdle-race.js
+++ javascript/hackerrank/the-hurdle-race.js
@@ -10,8 +10,9 @@ const hurdleRace = (k, height) => {
 test("hurdleRace", () => {
     const k0 = 4, k1 = 7;
     const height0 = [1, 6, 3, 5, 2], height1 = [2, 5, 4, 5, 2];
-    let result = hurdleRace(k0, height0);
-    expect(2).toEqual(result);
-    result = hurdleRace(k1, height1);
-    expect(0).toEqual(result);
+    const expected0 = 2, expected1 = 0;
+    const result0 = hurdleRace(k0, height0);
+    const result1 = hurdleRace(k1, height1);
+    expect(expected0).toEqual(result0);
+    expect(expected1).toEqual(result1);
 });
