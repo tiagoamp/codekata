68a6f8ceff71194e00f113b915c6f57842f679e2
tiagoamp <tiagoamp@gmail.com>
2021-04-27 11:02:26 -0300

    Solve JS code challenge

---
+++ javascript/hackerrank/halloween-sale.js
@@ -0,0 +1,24 @@
+// https://www.hackerrank.com/challenges/halloween-sale/problem
+
+function howManyGames(p, d, m, s) {
+    let nrOfGames = 0, budget = s, currGameVal = p;
+    while (currGameVal <= budget) {
+        nrOfGames++;
+        budget -= currGameVal;
+        currGameVal = ( (currGameVal-d) >= m ) ? (currGameVal - d) : m;
+    }
+    return nrOfGames;
+}
+
+
+test("sample 0", () => {
+    const p = 20, d = 3, m = 6, s = 80, expected = 6;
+    const result = howManyGames(p, d, m, s)
+    expect(result).toBe(expected);
+});
+
+test("sample 1", () => {
+    const p = 20, d = 3, m = 6, s = 85, expected = 7;
+    const result = howManyGames(p, d, m, s)
+    expect(result).toBe(expected);
+});
