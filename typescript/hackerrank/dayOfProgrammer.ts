68e075f8d04b5fc0ec7efe54ed62865ceb494a71
tiagoamp <tiagoamp@gmail.com>
2021-06-07 08:46:13 -0300

    Solve TS code challenge

---
+++ typescript/hackerrank/dayOfProgrammer.ts
@@ -0,0 +1,45 @@
+// https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+
+function dayOfProgrammer(year: number): string {
+    let day = 13;
+    if (year == 1918)
+        day += 13;  // lost days from Feb 1st to Feb 14th
+    if (isLeap(year))
+        day -= 1;
+    return `${day}.09.${year}`;
+}
+
+const isLeap = (year: number): boolean => {
+    if (year <= 1918)
+        return year % 4 == 0;
+    return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+}
+
+
+test("sample 0", () => {
+	const year = 2017;
+    const expected = "13.09.2017";
+    const result = dayOfProgrammer(year);
+	expect(result).toBe(expected);
+});
+
+test("sample 1", () => {
+	const year = 2016;
+    const expected = "12.09.2016";
+    const result = dayOfProgrammer(year);
+	expect(result).toBe(expected);
+});
+
+test("sample 2", () => {
+	const year = 1800;
+    const expected = "12.09.1800";
+    const result = dayOfProgrammer(year);
+	expect(result).toBe(expected);
+});   
+
+test("sample 11", () => {
+	const year = 1916;
+    const expected = "12.09.1916";
+    const result = dayOfProgrammer(year);
+	expect(result).toBe(expected);
+});
