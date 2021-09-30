d174648e81e067271efad9e375b92786d84287e3
tiagoamp <tiagoamp@gmail.com>
2021-09-30 07:53:53 -0300

    Refactor asserts

--- java/src/hackerrank/PlusMinus.java
+++ java/src/hackerrank/PlusMinus.java
@@ -1,5 +1,6 @@
 package hackerrank;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Locale;
@@ -31,9 +32,10 @@ public class PlusMinus {
 	void testPlusMinus() {
 		int[] input = {-4, 3, -9, 0, 4, 1};
 		String[] result = plusMinus(input);
-		assertEquals("0.500000", result[0]);
-		assertEquals("0.333333", result[1]);
-		assertEquals("0.166667", result[2]);
+		assertAll(() -> assertEquals("0.500000", result[0]), 
+				  () -> assertEquals("0.333333", result[1]), 
+				  () -> assertEquals("0.166667", result[2])
+		);
     }
 
 }
